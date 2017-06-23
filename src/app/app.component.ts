4086c6fb33d18d2a0345f8e38d2558afb3c09f7d
otrifo <otrifo@metadata.io>
2017-06-23 20:45:57 +0300

    push dist

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MdDialog} from '@angular/material';
+import {DialogComponent} from './components/dialog/dialog.component';
+import {Book} from './book';
+import {BooksStoreManagerService} from './managers/books-store-manager.service';
+import {Http} from '@angular/http';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  booksList: Book[];
+  bookListLength: number;
+
+  constructor(public dialog: MdDialog, private storeService?: BooksStoreManagerService, private http?: Http) {
+  }
+
+  ngOnInit(): void {
+    this.http.get('assets/json/booksData.json').subscribe(result => {
+      this.booksList = result.json().data;
+      this.storeService.booksList = this.booksList;
+      this.bookListLength = this.booksList.length;
+    })
+  }
+
+
+  addBook() {
+    this.bookListLength++;
+    this.storeService.bookValues = new Book(this.bookListLength, '', '', new Date);
+    const dialog = this.dialog.open(DialogComponent, {
+      width: '240px',
+      data: 'New book'
+    });
+    dialog.afterClosed().subscribe(result => {
+      if (result && result.dialogResult === 'true') {
+        this.booksList.push(this.storeService.bookValues);
+      }
+    });
+  }
 }
